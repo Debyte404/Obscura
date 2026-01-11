e356f848eec2cbf2ca8ba30fdb20a1cff23a5e82
Debyte404 <ankit.byte.404@gmail.com>
2026-01-12 02:29:44 +0530

    authentication

--- code/src/main.tsx
+++ code/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import { AuthProvider } from './contexts/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
